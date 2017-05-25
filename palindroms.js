935d46419d13bd635e355cbefc0f51c05f26935b
Анна Жукулова < >
2017-05-25 22:23:05 +0300

    Добавлен файл на поиск палиндромов в строке. JS 2

---
+++ palindroms.js
@@ -0,0 +1,14 @@
+/*Проверка строки на "палиндром" - читается ли она слева направо так же, как и справа налево*/
+
+function isPalindrome(s) {
+	var i = 0; 
+	s = s.toLowerCase();        // приводим к нижнему регистру
+	s = s.replace(/\s|,/g, ''); // убираем точки и пробелы, можно добавить "уборку" точек и пр.	
+	while (s[i] == s[s.length-1-i] && i < s.length) 
+		{i++;}	                // увеличиваем итератор, если соотв. эл-ты совпадают
+	return(i == s.length);      // 117 символов, не считая пробелы для читабельности кода
+}
+ 
+console.log(isPalindrome("level"));                  // true
+console.log(isPalindrome("levels"));                 // false
+console.log(isPalindrome("A car, a man, a maraca")); // true
